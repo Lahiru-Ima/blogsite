2bd41b55119ee381b63e1d184bebad9d74b66c96
LAPTOP-PD0DH7TF\Lahiru <lahiru.sl24@gmail.com>
2022-02-18 18:21:01 +0530

    Create table from json data

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 const url = 'https://jsonplaceholder.typicode.com/posts'
 
 const get_comment = async()=>{
+
     const response = await fetch(url);
-    const posts = await response.json() //data
+    const posts = await response.json() // data
 
-    
+    // const nodeList = [];
 
-    for (post of posts){
-        const{id,title,body} = post;    //destructure
-        //create div element
-        const div = document.createElement('div')
-        //create h1 element
-        const h1 = document.createElement('h1')
-        //create p element
-        const p = document.createElement('p')
+    // <div id={id}>
+    //     <h1>{title}</h1>
+    //     <p>{body}</p>
+    // </div>
 
-        div.id = id;
-        h1.innerHTML = title;
-        p.innerHTML = body;
-
-        div.appendChild(h1)
-        div.appendChild(p)
-
-        document.getElementById('root').appendChild(div)
-        break;
+    for(post of posts) {
+        const {id,title,body} = post; //destructure
         
-        // div.id = id;    //add id attr
-        // div.innerHTML = title;  //add innerHtml
-        // document.getElementById('root').appendChild(div)   //div#root child
-        // break
+        const tr = document.createElement('tr')
+        const td_title = document.createElement('td')
+        td_title.innerHTML = title;
+
+        const td_body = document.createElement('td')
+        td_body.innerHTML = body;
+
+        tr.appendChild(td_title)
+        tr.appendChild(td_body)
+
+        document.querySelector('table').appendChild(tr)
+
     }
+
 }
 get_comment()
 
